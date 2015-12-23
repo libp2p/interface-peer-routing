bbae42102fd6dba46b0e9872a3822632d97a5acb
wanderer <mjbecze@gmail.com>
2015-12-23 15:30:32 -0500

    added adPeer

--- tests/base-test.js
+++ tests/base-test.js
@@ -2,13 +2,22 @@ var Id = require('peer-id')
 
 module.exports.all = function (test, common) {
   test('Simple findPeers test', function (t) {
-    common.setup(test, function (err, pr) {
-      t.plan(3)
+    t.plan(4)
+    common.createPeer(function (err, pr1) {
       t.ifError(err)
-      pr.findPeers(Id.create().toBytes(), function (err, peerQueue) {
+      common.createPeer(function (err, pr2) {
         t.ifError(err)
-        t.equal(peerQueue.length >= 1, true)
-        common.teardown()
+
+        pr1.addPeer(pr2)
+        pr2.addPeer(pr1)
+
+        pr1.findPeers(Id.create().toBytes(), function (err, peerQueue) {
+          t.ifError(err)
+
+          t.equal(peerQueue.length, 1, 'should have one peer')
+          common.teardown(pr1)
+          common.teardown(pr2)
+        })
       })
     })
   })
